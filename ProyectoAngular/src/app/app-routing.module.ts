c7cd6ff1a4cd95cd5b1dde7eef4eabb35380009f
Douglas Castillo <douglascastillo087@gmail.com>
2022-06-26 20:35:37 -0600

    Modificaciones en el HomeComponent

--- ProyectoAngular/src/app/app-routing.module.ts
+++ ProyectoAngular/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GifsComponent } from './gifs/gifs.component';
+import { HomeComponent } from './home/home.component';
+import { StickersComponent } from './stickers/stickers.component';
 
 const routes: Routes = [
   {
-    path:'post/:id',
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'gifs',
     component:GifsComponent
+  },
+  {
+    path:'stickers',
+    component:StickersComponent
   }
 ];
 
